0a4e031e164e6db1be40f52d8510b722c35f3e57
Tanuja040621 <106739452+Tanuja040621@users.noreply.github.com>
2025-10-05 22:54:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 function firstChar(text) {
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] !== ' ') {
-      return text[i];
-    }
-  }
-  return '';
+ let trimmerText=text.trim();
+	return trimmerText.charAt(0);
 }
-
-// Do not change the code below
-// Uncomment the following line to show the prompt popup
-// const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
